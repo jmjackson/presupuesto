26d38f96d4b3211f8a47e59e6146f8ffcfd5b06c
Juan Hernandez <jarceluz@gmail.com>
2023-03-23 16:05:34 -0600

    First Iteration

---
+++ app/Controllers/Prueba.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+class Prueba extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function test(){
+        echo "hola mundo";
+    }
+}
